ba93a66cb76551cadfc78c403c034773a2c00ae8
Trung Dang <trungdangminh14012004@gmail.com>
2024-04-13 03:55:25 -0400

    stable_v4

---
+++ case_1/checkpoints/params_v4.py
@@ -0,0 +1,28 @@
+
+class Parameters:
+    def __init__(self):
+        self.max_pos = 200
+        self.params = {}
+        self.etf_params = {
+            "min_margin": 1,
+            "fade": 20,
+            "edge_sensitivity": 1,
+            "slack": 2
+        }
+        self.contract_params = {
+            "min_margin": 1,
+            "fade": 20,
+            "edge_sensitivity": .1,
+            "slack": 2
+        }
+        for c in ["EPT", "DLO", "MKU", "IGM", "BRV"]:
+            self.params[c] = self.contract_params
+        for c in ["SCP", "JAK"]:
+            self.params[c] = self.etf_params
+        self.spreads = [5, 10, 15]
+        self.level_orders = 2
+        self.etf_margin = 100
+        self.safety = False
+
+def get_params():
+    return Parameters()
